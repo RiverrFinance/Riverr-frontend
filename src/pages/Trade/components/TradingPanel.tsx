c52bc3ec7e65b8e6f163cbc67cb4413752965f82
calledAdo <destinyadokiye8@gmail.com>
2025-05-02 21:17:41 +0100

    revolve the trade issues

--- src/pages/Trade/components/TradingPanel.tsx
+++ src/pages/Trade/components/TradingPanel.tsx
@@ -28,7 +28,6 @@ export const TradingPanel: React.FC<TradingPanelProps> = ({
   const readWriteAgent = useAgent();
   const [error, setError] = useState<InputError>("");
   const [orderType, setOrderType] = useState<"Market" | "Limit">("Limit");
-  const [orderType, setOrderType] = useState<"Market" | "Limit">("Limit");
   const [tradeDirection, setTradeDirection] = useState<"Long" | "Short">(
     "Long"
   );
@@ -63,6 +62,7 @@ export const TradingPanel: React.FC<TradingPanelProps> = ({
     if (margin == "") {
       return "--";
     }
+
     return formatUnits(
       Number(margin) * Number(leverage),
       market.quoteAsset.decimals
@@ -116,8 +116,6 @@ export const TradingPanel: React.FC<TradingPanelProps> = ({
   return (
     <div className={`py-8 px-0 rounded-lg xxxl:space-y-8`}>
       <div className="mb-5 xxxl:mt-5 xxxl:space-y-8">
-    <div className={`py-8 px-0 rounded-lg xxxl:space-y-8`}>
-      <div className="mb-5 xxxl:mt-5 xxxl:space-y-8">
         {/* Order Type Selector */}
         <div className="relative p-1 mb-5 mx-3">
           <div className="flex relative z-10">
@@ -154,89 +152,92 @@ export const TradingPanel: React.FC<TradingPanelProps> = ({
         {/* Trade Direction Selector */}
         <div className="relative mx-4 border border-[#363c52] border-dashed rounded-lg border-opacity-40 bg-[#18191de9] w-32">
           <div className="flex z-10">
-        <div className="relative mx-4 border border-[#363c52] border-dashed rounded-lg border-opacity-40 bg-[#18191de9] w-32">
-          <div className="flex z-10">
-            {(["Long", "Short"] as const).map((type) => (
-              <button
-                type="button"
-                key={type}
-                onClick={() => setTradeDirection(type)}
-                className={`flex-1 py-2 text-sm font-medium relative  duration-300 ${
-                  tradeDirection === type
-                    ? "bg-[#0300adfd] rounded-md"
-                    : "border-none"
-                }`}
-              >
-                <span
-                  className={`relative z-10 ${
-                    tradeDirection === type
-                      ? "text-white"
-                      : "text-gray-400 hover:text-white"
-                  }`}
-                >
-                  {type}
-                </span>
-              </button>
-            ))}
+            <div className="relative mx-4 border border-[#363c52] border-dashed rounded-lg border-opacity-40 bg-[#18191de9] w-32">
+              <div className="flex z-10">
+                {(["Long", "Short"] as const).map((type) => (
+                  <button
+                    type="button"
+                    key={type}
+                    onClick={() => setTradeDirection(type)}
+                    className={`flex-1 py-2 text-sm font-medium relative  duration-300 ${
+                      tradeDirection === type
+                        ? "bg-[#0300adfd] rounded-md"
+                        : "border-none"
+                    }`}
+                  >
+                    <span
+                      className={`relative z-10 ${
+                        tradeDirection === type
+                          ? "text-white"
+                          : "text-gray-400 hover:text-white"
+                      }`}
+                    >
+                      {type}
+                    </span>
+                  </button>
+                ))}
+              </div>
+              {/* Sliding background */}
+              <div
+                className="absolute top-0 left-0 h-[calc(100%-0px)] w-[calc(50%-0px)] bg-[#0300ad18] border-2 border-dashed border-[#0300AD] transition-transform duration-300 ease-in-out rounded-md"
+                style={{
+                  transform: `translateX(${
+                    tradeDirection === "Short" ? "100%" : "0%"
+                  })`,
+                }}
+              />
+            </div>
           </div>
-          {/* Sliding background */}
-          <div
-            className="absolute top-0 left-0 h-[calc(100%-0px)] w-[calc(50%-0px)] bg-[#0300ad18] border-2 border-dashed border-[#0300AD] transition-transform duration-300 ease-in-out rounded-md"
-            style={{
-              transform: `translateX(${
-                tradeDirection === "Short" ? "100%" : "0%"
-              })`,
-            }}
-          /> */}
+
+          <div className="px-4 pt-4 space-y-14  xxxl:space-y-18">
+            <div className="px-4 pt-4 space-y-14  xxxl:space-y-18">
+              {/* Limit Price Input */}
+              {orderType == "Limit" ? (
+                <PriceInput
+                  long={tradeDirection == "Long"}
+                  readAgent={readAgent}
+                  market={market}
+                  value={limitPrice}
+                  setLimitPrice={setLimitPrice}
+                />
+              ) : (
+                <></>
+              )}
+
+              {/* margin Input */}
+              <MarginInput
+                value={margin}
+                market={market}
+                setMargin={setMargin}
+                setError={setError}
+                minCollateral={marketState.min_collateral}
+                readAgent={readAgent}
+              />
+
+              {/* Leverage Slider */}
+              <LeverageSlider
+                value={leverage}
+                maxLeverage={marketState.max_leveragex10 / 10}
+                setLeverage={setLeverage}
+              />
+
+              <div className="space-y-4 text-sm text-gray-400 pt-10 border-b border-gray-700 pb-5">
+                <div className="flex justify-between">
+                  <span>Total: {total()}</span>
+                  {market.quoteAsset.symbol}
+                </div>
+                <div className="flex justify-between">
+                  <span>Market: </span>
+                  {market.baseAsset.symbol}-{market.quoteAsset.symbol}{" "}
+                </div>
+              </div>
+
+              {/* <div className="mt-5 "> */}
+              <ActionButton currentError={error} onClick={openOrder} />
+              {/* </div> */}
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="px-4 pt-4 space-y-14  xxxl:space-y-18">
-      <div className="px-4 pt-4 space-y-14  xxxl:space-y-18">
-        {/* Limit Price Input */}
-        {orderType == "Limit" ? (
-          <PriceInput
-            long={tradeDirection == "Long"}
-            readAgent={readAgent}
-            market={market}
-            value={limitPrice}
-            setLimitPrice={setLimitPrice}
-          />
-        ) : (
-          <></>
-        )}
-
-        {/* margin Input */}
-        <MarginInput
-          value={margin}
-          market={market}
-          setMargin={setMargin}
-          setError={setError}
-          minCollateral={marketState.min_collateral}
-          readAgent={readAgent}
-        />
-
-        {/* Leverage Slider */}
-        <LeverageSlider
-          value={leverage}
-          maxLeverage={marketState.max_leveragex10 / 10}
-          setLeverage={setLeverage}
-        />
-
-        <div className="space-y-4 text-sm text-gray-400 pt-10 border-b border-gray-700 pb-5">
-          <div className="flex justify-between">
-            <span>Total: {total()}</span>
-            {market.quoteAsset.symbol}
-          </div>
-          <div className="flex justify-between">
-            <span>Market: </span>
-            {market.baseAsset.symbol}-{market.quoteAsset.symbol}{" "}
-          </div>
-        </div>
-
-        {/* <div className="mt-5 "> */}
-        <ActionButton currentError={error} onClick={openOrder} />
-        {/* </div> */}
       </div>
     </div>
   );
